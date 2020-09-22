5f9be57ac3e14a0b8fa8a9d1df010c775998d3d3
monica <monica@icounter.com>
2020-09-22 23:12:55 +0800

    shopping-cart组件

---
+++ src/components/shopping-cart.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <input type="text" @keydown.enter="addCourse" v-model="course">
+        <button @click="addCourse"> + Add</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'shopping-cart',
+        props:{
+            list:{
+                type: Array,
+                require:true,
+                default: () => []
+            }
+        },
+        data() {
+            return {
+                course: ''
+            }
+        },
+        methods: {
+            addCourse() {
+                this.$emit('add-course', this.course)
+                this.course = '';
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img
+      alt="Vue logo"
+      src="../assets/logo.png"
+    >
+    <hello-world msg="Welcome to Your Vue.js App" />
+    <shopping-cart
+      :list="list"
+      @add-course="addCourse"
+    />
+    <div v-if="list.length==0">
+      没有任何课程信息
+    </div>
+    <ul v-else style="list-style:none">
+      <li
+        v-for="item in list"
+        :key="item"
+        @click="selectedItem=item"
+        :class="{highlight:selectedItem==item}"
+      >
+        {{ item }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
+import ShoppingCart from '@/components/shopping-cart.vue';
 
 export default {
   name: 'Home',
+  //局部注册组件
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    ShoppingCart
+  },
+  data() {
+    return {
+      list: [],
+      selectedItem:''
+    }
+  },
+  methods: {
+    addCourse(val) {
+      this.list.push(val);
+    }
+  },
 }
 </script>
+<style scoped>
+  .highlight {
+    background: pink;
+  }
+</style>
